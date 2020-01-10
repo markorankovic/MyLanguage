898fbaf4dde0ad5e47cfeebbc68138ce99960ea0
Marko Rankovic <marko@markorankovic.com>
2020-01-10 21:06:08 +0000

    adding tdd

--- SwiftCompiler/src/testing/Tests.java
+++ SwiftCompiler/src/testing/Tests.java
@@ -1,14 +1,28 @@
 package testing;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void validFunctionDeclaration() {
+		Assert.assertTrue(false);
 	}
-
+	
+	@Test
+	public void validFunctionCall() {
+		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void validVariableDeclaration() {
+		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void validVariable() {
+		Assert.assertTrue(false);
+	}
+	
 }
